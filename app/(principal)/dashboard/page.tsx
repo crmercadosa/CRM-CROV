2282acea5cb5b353fde7afcca172d0e978e45485
crmercadosa <crmercadosa@ittepic.edu.mx>
2026-01-07 14:47:24 -0700

    Prueba push

--- app/(principal)/dashboard/page.tsx
+++ app/(principal)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useHeader } from '@/contexts/headerContexts';
 import clsx from 'clsx';
+import Link from 'next/link';
 
 export default function DashboardPage() {
 
